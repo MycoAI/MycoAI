9aaa21e9a5a33bb3f20a300ff7f0526c65240b80
Luuk Romeijn <luukromeijn@hotmail.nl>
2023-10-02 11:03:50 -0400

    Expanded example to show all options

--- example.py
+++ example.py
@@ -8,20 +8,29 @@ from mycoai.training import ClassificationTask
 
 # Some settings
 utils.set_output_dir('results') # Create results directory, save output there
+# utils.set_device('cuda') # To specify GPU use 
+# utils.VERBOSE = 1 # To turn on/off prints/plots
 
-# Data import
-train_data = data.DataPrep('trainset.fasta') # Parse file
-train_data = train_data.encode_dataset()
+# Data import & preprocessing
+train_data = data.DataPrep('trainset.fasta')
+train_data = train_data.class_filter('species', min_samples=5)
+train_data = train_data.sequence_length_filter()
+train_data = train_data.sequence_quality_filter()
+train_data, valid_data = train_data.encode_dataset(valid_split=0.2)
+
 # Use encoding scheme from train_data on the test set
 test_data = data.DataPrep('testset.fasta')
 test_data = test_data.encode_dataset(tax_encoder=train_data.tax_encoder)
 
 # Model definition
 arch = ResNet([2,2,2,2]) # = ResNet18
-model = ITSClassifier(arch, train_data.dna_encoder, train_data.tax_encoder,
-                                     fcn_layers=[128,50,64], output='chained')
+# This model will have a single output head and make genus-level predictions
+model = ITSClassifier(arch, train_data.dna_encoder, train_data.tax_encoder,  
+               target_levels=['genus'], fcn_layers=[128,20,64], output='single')
 
-# Train/test with weighted loss for class imbalance
-loss_functions = train_data.weighted_loss(torch.nn.CrossEntropyLoss)
-model, history = ClassificationTask.train(model,train_data, 100, loss_functions)
-result = ClassificationTask.test(model, test_data, loss_functions)
+# Train/test (optionally with weighted loss/sampling) 
+# sampler = train_data.weighted_sampler('genus')
+# loss_function = train_data.weighted_loss(torch.nn.CrossEntropyLoss, 
+#                                          sampler=sampler)
+model, history = ClassificationTask.train(model, train_data, valid_data, 100)
+result = ClassificationTask.test(model, test_data)
