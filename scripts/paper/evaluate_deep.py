561ec14c007c1f3b54603c6a689ace281b6c70c9
Luuk Romeijn <s2592800@nodelogin01.cm.cluster>
2024-02-16 10:18:27 +0100

    Added paper training scripts

--- scripts/paper/evaluate_deep.py
+++ scripts/paper/evaluate_deep.py
@@ -6,14 +6,17 @@ from mycoai import utils
 from mycoai.data import Data
 from mycoai.evaluate import Evaluator
 
-utils.set_device('cuda:1')
+utils.WANDB_PROJECT = 'Comparison'
 utils.PRED_BATCH_SIZE = 64
 
-model_folder = '/data/luuk/models/'
-data_folder = '/data/luuk/'
-results_folder = '/data/luuk/results/'
+model_folder = '/data1/s2592800/models/'
+data_folder = '/data1/s2592800/'
+results_folder = '/data1/s2592800/comparison/deep/'
 
-models = ['TODO']
+models = ['MycoAI-multi LS 0.02 0.02 0.02 0.02 0.02 0.0.pt',
+          'MycoAI-multi LS 0.0 0.0 0.0 0.0 0.0 0.1.pt',
+          'MycoAI-single LS 0.02 0.02 0.02 0.02 0.02 0.0.pt',
+          'MycoAI-single LS 0.0 0.0 0.0 0.0 0.0 0.1.pt',]
 
 testsets = ['test1.fasta',
             'test2.fasta',
@@ -37,21 +40,31 @@ for model_name in models:
         t1 = time.time()
         print(f'Runtime {name}: {t1-t0} s')
 
+        # No need to analyze performance on test set 3
+        if testset_name == 'test3.fasta':
+            del model, classification
+            torch.cuda.empty_cache()
+            continue
+
         # Analyze results
         reference = Data(data_folder + testset_name, allow_duplicates=True)
-        latent_space = model.latent_space(reference)
+        latent_space = None
+        if testset_name != 'test3.fasta':
+            latent_space = model.latent_space(reference)
         evaluator = Evaluator(classification, reference, model,
                               wandb_name=f'Results {name}')
         results = evaluator.test()
         results.to_csv(results_folder + 'results/' + name + '.csv')
-        for level in utils.LEVELS:
-            report = evaluator.detailed_report(level, train_src, latent_space)
-            report.to_csv(f'{results_folder}results/{name} {level} report.csv')
+        # report = evaluator.detailed_report('species', train_src, latent_space)
+        # report.to_csv(f'{results_folder}results/{name} species report.csv')
         evaluator.wandb_finish()
 
         del model, classification, latent_space, reference, evaluator
-        del results, report
+        del results # , report
         torch.cuda.empty_cache()
+
+models = ['MycoAI-multi LS 0.02 0.02 0.02 0.02 0.02 0.0.pt',
+          'MycoAI-multi LS 0.0 0.0 0.0 0.0 0.0 0.1.pt']
 
 # Compare hierarchical to standard label smoothing
 for model_name in models:
